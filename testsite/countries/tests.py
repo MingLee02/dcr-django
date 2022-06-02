c7810e6ee98e24738cedb988708f1de4456995c1
ming lee <ming.lee@hotmail.com>
2022-06-02 13:33:58 +0100

    Task 1 with test.

--- testsite/countries/tests.py
+++ testsite/countries/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Country, Region
+from .views import get_region_data
+
+
+class CountryTestCase(TestCase):
+    def setUp(self):
+        region = Region.objects.create(name='REGION')
+        Country.objects.create(name="country 1", population="10", region=region)
+        Country.objects.create(name="country 2", population="30", region=region)
+
+    def test_get_region_data(self):
+        """
+        Assert that get_region_data correctly returns number of countries tied to a region
+        plus the sum of the population
+        """
+        data = get_region_data()
+        data = data.first()
+
+        assert data == {'number_countries': 2, 'region__name': 'REGION', 'total_population': 40}
--- testsite/countries/views.py
+++ testsite/countries/views.py
@@ -1,8 +1,32 @@
+from django.db.models import Count, Sum
 from django.http import JsonResponse
+
+from .models import Country
+
+
+def get_region_data():
+    return Country.objects.all().values(
+        'region__name'
+    ).annotate(
+        number_countries=Count('region_id')
+    ).annotate(total_population=Sum('population')).order_by('region__name')
 
 
 def stats(request):
-    # TODO - Provide name, number_countries and total_population for each region
-    response = {"regions": []}
+    region_data = get_region_data()
+    if not region_data:
+        return JsonResponse({
+            "message": "No region data available"
+        })
+
+    response = {"regions": [
+            {
+                'name': data['region__name'],
+                'number_countries': data['number_countries'],
+                'total_population': data['total_population']
+            }
+            for data in region_data
+        ]
+    }
 
     return JsonResponse(response)
